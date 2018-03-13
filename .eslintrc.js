96d23ea98c54888ace5b3e8a0e55ca2e8854afd1
Oleg Bogdanov <olegbogdanov86@gmail.com>
2018-03-14 07:43:23 +0900

    update eslint

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  "settings": {
+    "import/resolver": "webpack"
+  },
+  // add your custom rules here
+  rules: {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+
+    'semi': 'off',
+    'indent': 'off',
+    'valid-typeof': 'off',
+    'no-useless-escape': 'off',
+    'no-unneeded-ternary': 'off',
+    'space-before-function-paren': 'off',
+    'no-extend-native': 'off'
+  },
+  globals: {}
+};
